10011a0d357b708380d928abcb767fe20c6c87ae
Malaquia24 <arthur.matheus0607@gmail.com>
2023-11-16 16:20:56 -0300

    update

---
+++ App/check.js
@@ -0,0 +1,133 @@
+const App = () => {
+    return(
+        <div>
+            <nav class="cabecalho navbar navbar-expand-lg">
+                <div class="container m-3">
+                    <a class="navbar-brand " href="./index/index.html"><img class="img-fluid w-50 p-3 " src="../img/Logo.png"></img></a>
+                </div>
+            </nav>      
+            <div>
+                <h1 class="d-flex justify-content-center">CHECKLIST</h1>
+                <div class="m-5">
+                    <h3>1. Identificação e Documentação:</h3>
+                        <div class="m-1">
+                        <input  type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> A máquina está identificada corretamente.</label>
+                        </div>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> O manual de operação está acessível e atualizado.</label>
+                        </div>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Conheço os procedimentos de segurança específicos para esta máquina.</label>
+                        </div>
+                    <h3>2. Proteções e Dispositivos de Segurança:</h3>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification">  As proteções nas áreas perigosas estão em bom estado.</label>
+                        </div>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Conheço a localização e o funcionamento dos dispositivos de parada de emergência.</label>
+                        </div>
+                    <h3>3. Sinalização de Segurança:</h3>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> As sinalizações de segurança estão visíveis e compreensíveis.</label>
+                        </div>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> As cores de sinalização estão de acordo com as normas.</label>
+                        </div>
+                    <h3>4. Controle de Emissões e Resíduos:</h3>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Os sistemas de controle de emissões estão funcionando..</label>
+                        </div>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Sei como lidar com resíduos de polímeros de forma segura.</label>
+                        </div>
+                    <h3>5. Manutenção e Inspeção:</h3>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Realizo verificações visuais regulares na máquina.</label>
+                        </div>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Reporto qualquer sinal de desgaste, vazamento ou problema elétrico.</label>
+                        </div>
+                    <h3>6. Capacitação e Treinamento:</h3>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Recebi treinamento sobre o manuseio seguro de polímeros</label>
+                        </div>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Mantenho registros do meu treinamento.</label>
+                        </div>
+                    <h3>7. Ruído e Emissões Térmicas:</h3>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Utilizo equipamentos de proteção auricular, se necessário.</label>
+                        </div>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Conheço as medidas para controlar ruído e emissões térmicas.</label>
+                        </div>
+                    <h3>8. EPIs (Equipamentos de Proteção Individual):</h3>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Utilizo os EPIs recomendados para a minha segurança.</label>
+                        </div>
+                        <div class="m-1">
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Verifico regularmente a condição dos meus EPIs.</label>
+                        </div>
+                    <h3>9. Limpeza e Higienização:</h3>
+                        <div>
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Sigo os procedimentos de limpeza e higienização.</label>
+                        </div>
+                        <div>
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Comunico qualquer irregularidade na limpeza.</label>
+                        </div>
+                    <h3>10. Controle de Temperatura e Pressão:</h3>
+                        <div>
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Estou ciente dos controles de temperatura e pressão na máquina.</label>
+                        </div>
+                        <div>
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification">  Sei como reagir em caso de variações inesperadas.</label>
+                        </div>
+                    <h3>11. Outros Itens Específicos e Gerais:</h3>
+                        <div>
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Verifico itens específicos da máquina, como controle de umidade, conforme necessário.</label>
+                        </div>
+                        <div>
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Registro quaisquer incidentes ou situações de risco.</label>
+                        </div>
+                        <div>
+                        <input type="checkbox" id="machineIdentification"></input>
+                        <label for="machineIdentification"> Participação ativa na implementação de ações corretivas.</label>
+                        </div>
+                </div>
+                <div>
+                    <input type="checkbox" id="machineIdentification"></input>
+                    <label for="machineIdentification">Eu, <strong>Breatriz</strong>, declaro que li e compreendi o Checklist de Segurança para Máquinas de Polímeros e concordo em cumprir todas as medidas de segurança especificadas.</label>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+    )
---
+++ Relatar/index.js
@@ -0,0 +1,29 @@
+const App = () => {
+    return(
+        <div>
+            <nav class="cabecalho navbar navbar-expand-lg">
+                <div class="container m-3">
+                    <a class="navbar-brand " href="./index/index.html"><img class="img-fluid w-50 p-3 " src="../img/Logo.png"></img></a>
+                </div>
+            </nav>
+
+            <h3 class="ms-5">Relatar problema</h3>
+            <form class="ms-5">
+            <div class="mb-5">
+            <label class="form-label"  for="exampleFormControlTextarea1" >O que aconteceu?</label>
+            <input class="formulario form-control" id="exampleFormControlTextarea1" aria-describedby="emailHelp"></input>
+            </div>
+            <div class="mb-3">
+            <label class="form-label"  for="exampleFormControlTextarea1" >Como o problema foi resolvido?</label>
+            <div id="emailHelp" class="form-text">Caso não tenha sido solucionado ainda, informe e justifique.</div>
+            <input class="formulario form-control" id="exampleFormControlTextarea1" aria-describedby="emailHelp"></input>
+            </div>
+            </form>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+    )
